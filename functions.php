b7af845c69e67021fb4b0e2aa981b307efe77bc4
Kirill Tobola <kirilltobola@gmail.com>
2022-10-30 13:46:50 +0800

    add error view

--- functions.php
+++ functions.php
@@ -2,7 +2,13 @@
 
 function abort($status_code=404): void
 {
+    $code_desc = [
+        '404' => "Запрашиваемый ресурс не найден.",
+        '500' => "Ошибка на сервере"
+    ];
+
     http_response_code($status_code);
-    require "resources/views/{$status_code}.php";
+    $desc = $code_desc[$status_code];
+    require "resources/views/error.php";
     die();
 }
---
+++ resources/views/error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;300;400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/resources/css/404.css">
+</head>
+<body>
+<div class="status-code_container">
+    <span class="status-code"><?= $status_code ?></span>
+    <span class="error-desc"><?= $desc ?></span>
+</div>
+</body>
+</html>
